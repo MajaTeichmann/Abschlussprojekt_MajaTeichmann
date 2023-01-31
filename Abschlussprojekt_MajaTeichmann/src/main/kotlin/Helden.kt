300f207a6671e74b31260f1aba2202a868ec9eac
MajaTeichmann <121936594+MajaTeichmann@users.noreply.github.com>
2023-01-31 18:55:12 +0100

    Delete Abschlussprojekt_MajaTeichmann directory

--- Abschlussprojekt_MajaTeichmann/src/main/kotlin/Helden.kt
+++
@@ -1,7 +0,0 @@
-open class Helden(var heldenArt: String, var name: String, var hp: Int) {
-
-    open var faehigkeiten = mutableListOf<String>()
-    open fun showFaehigkeiten(){
-        println(faehigkeiten)
-    }
-}
